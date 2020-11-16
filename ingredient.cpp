db5a0622dc1099a64a9483dc11238a8ef98dce3d
IR14 <59391583+IR14@users.noreply.github.com>
2020-11-16 16:08:30 +0300

    Update ingredient.cpp

--- ingredient.cpp
+++ ingredient.cpp
@@ -5,18 +5,25 @@
 Ingredient::Ingredient() {
     name = default_name;
     unit = default_unit;
+    operation = default_operation;
+    time = default_time;
     quantity = default_quantity;
 }
 
-Ingredient::Ingredient(const std::string &name, const std::string &unit, const int &quantity) {
+Ingredient::Ingredient(const std::string &name, const std::string &unit, const std::string &operation, const int &time,
+                       const int &quantity) {
     this->name = name;
     this->unit = unit;
+    this->operation = operation;
+    this->time = time;
     this->quantity = quantity;
 }
 
 Ingredient::Ingredient(const Ingredient &copy) {
     name = copy.name;
     unit = copy.unit;
+    operation = copy.operation;
+    time = copy.time;
     quantity = copy.quantity;
 }
 
@@ -32,6 +39,20 @@ void Ingredient::set_unit(const std::string &unit) {
         this->unit = unit;
     else
         this->unit = default_unit;
+}
+
+void Ingredient::set_operation(const std::string &operation) {
+    if (operation.length() > 0)
+        this->operation = operation;
+    else
+        this->operation = default_operation;
+}
+
+void Ingredient::set_time(const int &time) {
+    if (duration > 0)
+        this->time = time;
+    else
+        this->time = default_time;
 }
 
 void Ingredient::set_quantity(const int &quantity) {
