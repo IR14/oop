e8e718d622c7a8b3d4d2b22e6f18d9c37f51794f
IR14 <59391583+IR14@users.noreply.github.com>
2020-11-16 16:07:51 +0300

    Update ingredient.h

--- ingredient.h
+++ ingredient.h
@@ -5,25 +5,34 @@
 
 class Ingredient {
 public:
-    inline static const std::string default_name = "pickle";
-    inline static const std::string default_unit = "thing";
+    static const std::string default_name = "pickle";
+    static const std::string default_unit = "thing";
+    static const std::string default_operation = "cut";
+    static const int default_time = 10;
     static const int default_quantity = 1;
 
     Ingredient();
-    Ingredient(const std::string &name, const std::string &unit, const int &quantity); //init
+    Ingredient(const std::string &name, const std::string &unit, const std::string &operation, const int &time, const int &quantity); //init
     Ingredient(const Ingredient &copy); //cp
+    ~Ingredient() = default; //rm
 
     std::string get_name() const { return name; }
     std::string get_unit() const { return unit; }
+    std::string get_operation() const { return operation; }
+    int get_time() const { return time; }
     int get_quantity() const { return quantity; }
 
     void set_name(const std::string &name);
     void set_unit(const std::string &unit);
+    void set_operation(const std::string &operation);
+    void set_time(const int &time);
     void set_quantity(const int &quantity);
 
 private:
     std::string name;
     std::string unit;
+    std::string operation;
+    int time;
     int quantity;
 };
 
