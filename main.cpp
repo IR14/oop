48096fae20bc409abf42124e2c543be12bee6b53
IR14 <59391583+IR14@users.noreply.github.com>
2020-11-16 16:06:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include "tests.h"
 #include "ingredient.h"
+#include "recipe.h"
 #include <string>
 
 int main() {
     testIngredient();
 
-    Ingredient ingredient("melon", "l", 6);
+    Ingredient ingredient("melon", "l", "split", 35, 6);
 
     std::cout << ingredient.get_name() << std::endl;
     std::cout << ingredient.get_unit() << std::endl;
+    std::cout << ingredient.get_operation() << std::endl;
+    std::cout << ingredient.get_time() << std::endl;
     std::cout << ingredient.get_quantity() << std::endl;
 
     ingredient.set_name("pumpkin");
     ingredient.set_unit("mgr");
+    ingredient.set_operation("fire");
+    ingredient.set_time(13);
     ingredient.set_quantity(666);
 
     std::cout << ingredient.get_name() << std::endl;
     std::cout << ingredient.get_unit() << std::endl;
+    std::cout << ingredient.get_operation() << std::endl;
+    std::cout << ingredient.get_time() << std::endl;
     std::cout << ingredient.get_quantity() << std::endl;
 
     return 0;
